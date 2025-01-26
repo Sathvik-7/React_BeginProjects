5cd3a97322de089b473b9447fc2fd39968c1b5f4
Sathvik-7 <chilluvurisathvik@gmail.com>
2025-01-26 10:53:45 -0600

    Search Bar

---
+++ SearchBar/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HiddenSearch from './component/HiddenSearch'
+
+const App = () => {
+  return (
+    <div>
+      <HiddenSearch/>
+    </div>
+  )
+}
+
+export default App
